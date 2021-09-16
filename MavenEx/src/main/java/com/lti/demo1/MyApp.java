ee2b4c65a191e1b5de3f43c50f0da4a0cde89a3b
AnishTiwari <anishtiwari@git.om>
2021-09-16 16:41:26 +0530

    Jenkins Git Maven Project

---
+++ MavenEx/src/main/java/com/lti/demo1/MyApp.java
@@ -0,0 +1,12 @@
+package com.lti.demo1;
+
+public class MyApp {
+	public static String show() {
+		return "Hi All";
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(show());
+	}
+
+}
